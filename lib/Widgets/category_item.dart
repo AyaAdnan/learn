72e57b1e96c192b218471799e7e214b5a00f10f0
AyaAdnan <aya.adnan1804a@coeduw.uobaghdad.edu.id>
2021-03-29 20:25:49 +0300

    category_meals_screen

--- lib/Widgets/category_item.dart
+++ lib/Widgets/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/screens/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
 
@@ -6,12 +7,23 @@ class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
   CategoryItem(this.id,this.title,this.color);
+ void selectCategory(BuildContext ctx){
+   Navigator.of(ctx).pushNamed(CategoryMealsScreen.routeName,
+   arguments: {
+    'id':id,
+    'title':title,
+   });
 
+
+ }
 
   @override
   Widget build(BuildContext context) {
+
     return InkWell(
-      onTap: (){},
+      onTap: ()=>selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(20),
       child: Container(
         padding: EdgeInsets.all(15),
         child: Text(
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/screens/category_meals_screen.dart';
 import 'screens/categoreis_screen.dart';
 
 void main() {
@@ -31,7 +32,12 @@ class MyApp extends StatelessWidget {
         )
 
       ),
-      home: CategoriesScreen(),
+
+      routes: {
+        '/': (context)=>CategoriesScreen(),
+        CategoryMealsScreen.routeName:(context)=>CategoryMealsScreen(),
+
+      },
     );
   }
 }
