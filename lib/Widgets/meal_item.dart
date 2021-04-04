d94a1d4af780e38e9901ccc1203dcc9d31e7ba7a
AyaAdnan <aya.adnan1804a@coeduw.uobaghdad.edu.id>
2021-04-04 19:54:01 +0300

    add title & duration

--- lib/Widgets/meal_item.dart
+++ lib/Widgets/meal_item.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:meal_app/models/meal.dart';
 
@@ -14,6 +15,25 @@ class MealItem extends StatelessWidget {
       @required this.duration,
       @required this.complexity,
       @required this.affordability});
+
+  String get complexitytext{             // هذه الداله من نوع enum تستخدم لارجاع قيمة واحدة من نوع String
+    switch(complexity){
+      case Complexity.Simple : return 'Simple'; break;
+      case Complexity.Challenging : return 'Chalenging'; break;
+      case Complexity.Hard : return 'Hard'; break;
+      default : return 'Unknown' ; break;
+
+    }
+  }
+
+  String get affordabliityText{
+    switch(affordability){
+      case Affordability.Affordable :return 'Affordable'; break;
+      case Affordability.Luxurious :return 'Luxurios'; break;
+      case Affordability.Pricey : return 'Pricey';break;
+
+    }
+  }
 
   selectMeal() {}
 
@@ -42,8 +62,52 @@ class MealItem extends StatelessWidget {
                     width: double.infinity,
                     fit: BoxFit.cover,
                   ),
-                )
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 220,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(vertical: 5,horizontal: 20),
+                    child: Text(
+                      title,
+                      style: TextStyle(fontSize: 26, color: Colors.white),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                ),
               ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.schedule),
+                      SizedBox(width: 6,),
+                      Text("$duration min"),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.work),
+                      SizedBox(width: 6,),
+                      Text("$complexitytext"),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.attach_money_outlined),
+                      SizedBox(width: 6,),
+                      Text("$affordabliityText"),
+                    ],
+                  ),
+                ],
+              ),
             )
           ],
         ),
